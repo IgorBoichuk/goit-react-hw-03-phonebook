24c04af59b844981d773f60d8af198099b3b3fd8
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-03-13 10:03:10 +0200

    Copy files

---
+++ src/components/App.jsx
@@ -0,0 +1,69 @@
+import { Component } from 'react';
+import { ContactForm } from './contact-form/ContactForm';
+import { ContactList } from './contact-list/ContactList';
+import { Filter } from './filter/Filter';
+import { nanoid } from 'nanoid';
+
+import style from './style.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const unicID = nanoid();
+
+    this.setState({
+      ...this.state,
+      contacts: [...this.state.contacts, { id: unicID, ...data }],
+    });
+  };
+
+  changeFilterInput = filter => {
+    this.setState({ ...this.state, filter });
+  };
+
+  handleDeleteContact = id => {
+    let deleteItem = this.state.contacts;
+    this.state.contacts.forEach((item, index) => {
+      if (item.id === id) {
+        deleteItem.splice(index, 1);
+      }
+    });
+    this.setState({ ...this.state, contacts: [...deleteItem] });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          width: '400px',
+          display: 'flex',
+          flexDirection: 'column',
+          // justifyContent: 'center',
+          marginLeft: '150px',
+          color: '#010101',
+        }}
+      >
+        <div>
+          <h1 className={style.title}>Phonebook</h1>
+          <ContactForm
+            formSubmitHandler={this.formSubmitHandler}
+            contacts={this.state.contacts}
+          />
+          <Filter changeFilterInput={this.changeFilterInput} />
+        </div>
+        <h2 className={style.text}>Contacts</h2>
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          handleDeleteContact={this.handleDeleteContact}
+        />
+        {/* <Input /> */}
+      </div>
+    );
+  }
+}
